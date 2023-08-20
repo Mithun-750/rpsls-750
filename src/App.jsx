d97fd8b7759e79b9b44b7f1e56e370c4bc92f9c4
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2023-08-20 23:04:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 import Header from './Component/Header/Header';
 import MoveContainer from './Component/MoveContainer/MoveContainer';
@@ -11,16 +11,6 @@ function App() {
   const [CMove, setCMove] = useState("")
   const [Pmove, setPmove] = useState("")
   const [Status, setStatus] = useState("")
-
-  useEffect(() => {
-    console.log('Hi1')
-
-    return () => {
-      console.log('Hi2')
-
-    }
-  }, [])
-
 
   return (
     <>
